e0608d4e1f0358fbec027559c167a6d41fc57b9f
ericson matheus <ericson.matheus.2016@gmail.com>
2022-06-22 14:42:15 -0300

    create sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,9 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
+import { Event } from "./pages/Event";
 
 function App() {
 
-  /*OUTRO FORMATO PARA FAZER REQUISIÇÃO NA API*/
-  // useEffect(() => {
-  //   client.query({
-  //     query: GET_LESSONS_QUERY
-  //   }).then(response => console.log(response))
-  // }, [])
-
-  interface Lesson {
-    id: string;
-    title: string;
-  }
-
-  const { data } = useQuery<{ lessons: Lesson[]}>(GET_LESSONS_QUERY);
-
-  console.log(data)
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>;
-      })}
-    </ul>
+    < Event />
   )
 }
 
